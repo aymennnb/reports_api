2a412c964bc8cc5c6b73aadd6ca81ff3782be319
aymennnb <aymennabaoui1229@gmail.com>
2026-04-13 17:52:14 +0100

    setup project with Express API structure

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
